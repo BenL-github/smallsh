a7080502114755cf8a9a6ed4fec394a785b318f1
Benny Li <libenn@os1.engr.oregonstate.edu>
2022-07-07 23:20:55 -0700

    finish arg parsing and start exec

--- smallsh.c
+++ smallsh.c
@@ -1,17 +1,18 @@
 #define _GNU_SOURCE
+#include <sys/wait.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 void 
-execute(char** argv){
+execute(char **argv){
     if(execvp(*argv, argv) < 0) perror("Exec failure"); exit(1);
 }
 
 int 
 main(int argc, char* argv[]){
-    struct args{
+    struct user_input{
         // command name
         char* command;
         // array of string pointers 
@@ -32,6 +33,7 @@ main(int argc, char* argv[]){
         char *input = NULL;
         size_t len = 2048;
         getline(&input, &len, stdin);
+        input[strlen(input)-1] = '\0';
 
         // initialize an array of pointers for all input arguments
         // arguments[0] = command
@@ -44,6 +46,7 @@ main(int argc, char* argv[]){
 
         // count total number of args
         int total_args = 0;
+        int total_command_args = 0;
     
         // variables to mark indices of infile & outfiles
         // -1 means user did not input an in/outfile
@@ -63,27 +66,37 @@ main(int argc, char* argv[]){
         while(arg){
             if(strcmp(arg, "\n") != 0){
                 // check if <
-                if(strcmp(arg, "<")){
-                    infile_index = index + 1;
-
+                if(strcmp(arg, "<") == 0){ 
+                    infile_index = index + 1; 
+                    total_command_args -= 2;
                 }
                 // check if >
-                if(strcmp(arg, ">")){ 
-                    outfile_index = index + 1;
+                if(strcmp(arg, ">") == 0){ 
+                    outfile_index = index + 1; 
+                    total_command_args -= 2;
                 }
                 // check if &
-                if(strcmp(arg, "&")){ 
-                    background = 1;
+                if(strcmp(arg, "&") == 0){ 
+                    background = 1; 
+                    total_command_args--;
                 }
-
                 arguments[index] = arg;
                 index++;
                 total_args++;
             }
             arg = strtok(NULL, " ");
         }
+
         printf("Total args: %d\n", total_args);
-    
+        total_command_args += total_args;
+        // get the command args (arguments for the command)
+        char *command_args[total_command_args + 1];
+        memcpy(command_args, &arguments, (total_command_args+1)*sizeof(char*));
+        command_args[total_command_args] = NULL;
+
+        for(int i = 0; i < total_command_args; i++){
+            printf("%s\n", command_args[i]);
+        }
         // check if command is exit
 
         // check if command is cd 
@@ -91,7 +104,21 @@ main(int argc, char* argv[]){
         // check if command is status 
 
         // other commands 
+        pid_t spawnPid = -5;
+        int childExitStatus = -5;
 
-
+        spawnPid = fork();
+        switch(spawnPid){
+            case -1: { perror("Hull Breach\n"); exit(1); break; }
+            case 0: {
+                execute(command_args);
+                perror("Child: exec failed\n");
+                exit(2); break;
+            }
+            default: {
+                pid_t actualPid = waitpid(spawnPid, &childExitStatus, 0);
+                break;
+            }
+        }
     }
 }
