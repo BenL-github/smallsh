f942c3dc62d05783cc42ad3e9716411f689cd562
Benny Li <libenn@os1.engr.oregonstate.edu>
2022-07-19 13:07:55 -0700

    implement redirect

--- smallsh.c
+++ smallsh.c
@@ -9,26 +9,8 @@
 
 static int last_status = 0;
 
-void 
-execute(char **argv){
-    if(execvp(*argv, argv) < 0) perror("Exec failure"); exit(1);
-}
-
 int 
 main(int argc, char* argv[]){
-    struct user_input{
-        // command name
-        char* command;
-        // array of string pointers 
-        char** arguments;
-        // input file name 
-        char* infile;
-        // output file name 
-        char* outfile;
-        // 1 if & was found, 0 if not 
-        int background;
-    };
-    
     while(1){
         printf(": ");
         fflush(stdout);
@@ -69,34 +51,38 @@ main(int argc, char* argv[]){
         if(strcmp(arg, "#") == 0) continue;
 
         while(arg){
-            if(strcmp(arg, "\n") != 0){
-                // check if <
-                if(strcmp(arg, "<") == 0){ 
-                    infile_index = index + 1; 
-                    total_command_args -= 2;
-                }
-                // check if >
-                if(strcmp(arg, ">") == 0){ 
-                    outfile_index = index + 1; 
-                    total_command_args -= 2;
-                }
-                // check if &
-                if(strcmp(arg, "&") == 0){ 
-                    background = 1; 
-                    total_command_args--;
-                }
-                arguments[index] = arg;
-                index++;
-                total_args++;
+            
+            // check if <
+            if(strcmp(arg, "<") == 0){ 
+                infile_index = index + 1; 
+                total_command_args -= 2;
             }
+            // check if >
+            if(strcmp(arg, ">") == 0){ 
+                outfile_index = index + 1; 
+                total_command_args -= 2;
+            }
+            // check if &
+            if(strcmp(arg, "&") == 0){ 
+                background = 1; 
+                total_command_args--;
+            }
+            arguments[index] = arg;
+            index++;
+            total_args++;
+            
             arg = strtok(NULL, " ");
         }
 
         // printf("Total args: %d\n", total_args);
+        
+        // get the arguments for the command, not including
+        // the redirect operators, redirect files, and '&'
+        // >>> these arguments are for the exec() funciton <<<
         total_command_args += total_args;
-        // get the command args (arguments for the command)
         char *command_args[total_command_args + 1];
         memcpy(command_args, &arguments, (total_command_args+1)*sizeof(char*));
+        // exec() requires the last value to be a null pointer 
         command_args[total_command_args] = NULL;
 
         // for(int i = 0; i < total_command_args; i++){
@@ -132,23 +118,28 @@ main(int argc, char* argv[]){
             case -1: { perror("Hull Breach\n"); exit(1); break; }
             case 0: {
                 if(outfile_index > -1){
-                    int outfile = open(command_args[outfile_index], O_WRONLY ,"w");
-                    if(dup2(outfile, STDIN_FILENO) == -1){
-                        fprintf(stderr, "Cannot open for %s output\n", command_args[outfile_index]);
+                    int outfile = open(arguments[outfile_index], O_WRONLY | O_CREAT,"w");
+                    if(dup2(outfile, STDOUT_FILENO) == -1){
+                        fprintf(stderr, "Cannot open for %s output\n", arguments[outfile_index]);
+                        last_status = 1;
+                        close(outfile);
                         break;
                     }
                 } 
                 if(infile_index > -1){
-                    int infile = open(command_args[infile_index], O_RDONLY ,"r");
-                    if(dup2(infile, STDOUT_FILENO) == -1){
-                        fprintf(stderr, "Cannot open %s for input\n", command_args[infile_index]);
+                    int infile = open(arguments[infile_index], O_RDONLY ,"r");
+                    if(dup2(infile, STDIN_FILENO) == -1){
+                        fprintf(stderr, "Cannot open %s for input\n", arguments[infile_index]);
+                        last_status = 1;
+                        close(infile);
                         break;
                     }
                 } 
-                execute(command_args);
-                last_status = 1;
-                perror("Child: exec failed\n");
-                exit(2); break;
+                if(execvp(*command_args, command_args) < 0){
+                    last_status = 1;
+                    perror("Child: exec failed\n");
+                }
+                break;
             }
             default: {
                 pid_t actualPid = waitpid(spawnPid, &childExitStatus, 0);
